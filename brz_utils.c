50290e80e71919da86399cf9de259ddde96a1a24
Carter J. Bastian <carter.bastian1@gmail.com>
2017-02-17 11:57:46 -0500

    Add initial utility functions
    Initial functions include:
    * `fatal_error` - exiting a program with an error code and message
    * `safe_malloc` - error checking and zeroing out memory allocation

---
+++ brz_utils.c
@@ -0,0 +1,60 @@
+/*
+ * brz_utils.c      Implementations of utilities defined in brz_utils.h
+ *
+ * Author:          Carter J. Bastian
+ *                  February, 2017
+ *
+ * Description:
+ *    Implementations of personal c utility functions declared in brz_utils.h
+ *    Includes:
+ *      - Error handling
+ *      - Error-checking wrappers for memory allocation
+ *
+ * Attributions:
+ *    Some of these functions are adapted (or directly reused) versions of
+ *    functions originally presented in "hacking; the art of exploitation" by
+ *    Jon Erickson.
+ */
+
+// System Includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Constants
+const char *error_prefix = "[!!] Fatal Error: ";
+
+// Public Function Definitions
+/*
+ * fatal_error - prints error message and exits with an error code
+ *
+ * Args:
+ *  int exit_value - error value the process should return
+ *  char *message - the error message to print to the console
+ *
+ */
+void fatal_error(int exit_value, char *message) {
+  char error_message[ERROR_MESSAGE_MAX_LENGTH];
+  int prefix_length = strlen(error_prefix);
+
+  // Format and print an error message with perror
+  strncpy(error_message, error_prefix, prefix_length);
+  strncat(error_message, message, ERROR_MESSAGE_MAX_LENGTH - prefix_length);
+  perror(error_message);
+
+  exit(exit_value);
+}
+
+void *safe_malloc(unsigned int size) {
+  void *ptr;
+
+  // Check for memory allocation success
+  ptr = malloc(size);
+  if (ptr == NULL)
+    fatal_error(-1, "Memory allocation failed");
+
+  // Zero out freshly allocated memory
+  memset(ptr, 0, (size_t) size);
+
+  return ptr;
+}
---
+++ brz_utils.h
@@ -0,0 +1,27 @@
+/*
+ * brz_utils.h      Function declarations for a brz-utilities
+ *
+ * Author:      Carter J. Bastian
+ *              February, 2017
+ *
+ * Description:
+ *    Declarations for my personal c utility functions.
+ *
+ * Attributions:
+ *    Some of these functions are adapted (or directly reused) versions of
+ *    functions originally presented in "hacking; the art of exploitation" by
+ *    Jon Erickson.
+ */
+#ifndef BRZ_UTILS_H_
+#define BRZ_UTILS_H_
+
+// Constant and Macro Definitions
+#define ERROR_MESSAGE_MAX_LENGTH 100
+
+// Public Function Declarations
+void fatal_error(int exit_value, char *message);
+
+void *safe_malloc(unsigned int size);
+
+#endif // BRZ_UTILS_H_
+
