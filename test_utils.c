2aa6fc3a99f594b011dfc265ac78c2a258fe357f
Carter J. Bastian <carter.bastian1@gmail.com>
2017-02-18 14:01:27 -0500

    Initialize the testing framework
    Write skeleton code for reusable unit testing framework.

---
+++ test_utils.c
@@ -0,0 +1,34 @@
+/*
+ * test_utils.c      Unit tests for test_utilities
+ *
+ * Author:      Carter J. Bastian
+ *              February, 2017
+ *
+ * Description:
+ *    Builds a simple unit testing framework for my utilities.
+ *    To add a new test:
+ *      - Add the test function declaration
+ *      - Add a reference to the test and its name in global variable test_list
+ *      - Create the function definition that is the test
+ */
+// System Includes
+#include <stdio.h>
+
+// Local Includes
+#include "brz_util.h"
+
+// Private Function Declarations
+void fail_with_fatal_error();
+
+// Public Function Definitions
+
+// Private Function Definitions
+
+// Global Variables
+
+/*
+ * When running as main, run all of the unit tests and assert their success
+ */
+int main(int argc, char **argv) {
+
+}
